d707081bd97a649b29b411e861b09d25feccb97e
TheHoltz <holtzs.william@gmail.com>
2023-08-22 19:53:39 -0300

    feat: :sparkles: navbar responsiveness

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -18,7 +18,7 @@ const NavbarLink: FC<NavbarLinkProps> = ({ href, children }) => {
 	const isActive = pathname === href;
 	return (
 		<Link
-			className={`font-robotoMono hover:font-bold pb-4 px-3 text-neutral-500 ${isActive ? "!text-black font-bold border-b-2 border-black" : ""
+			className={`whitespace-nowrap font-robotoMono hover:font-bold pb-4 px-3 text-neutral-500 ${isActive ? "!text-black font-bold border-b-2 border-black" : ""
 				}`}
 			href={href}
 		>
@@ -56,25 +56,29 @@ const Navbar: FC = () => {
 	];
 
 	return (
-		<header className="w-full top-0 left-0 bg-background text-text border-b">
-			<nav className="">
-				<div className="flex flex-col items-center justify-between">
+		<header className="w-full bg-background text-text border-b">
+			<nav>
+				<div className="flex flex-col items-center justify-between w-full">
 					<div className="text-lg font-semibold bg-black w-full h-full py-4 text-white text-center">
 						<Link className="flex items-center gap-4 cursor-pointer justify-center" href="/">
 							<BundlrIcon /> <span className="">Provenance Toolkit</span>
 						</Link>
 					</div>
 					<div className="h-[1px] bg-black w-full" />
-					<div className="flex space-x-8 pt-4">
-						{NAV_LINKS.map((link, index) => (
-							<NavbarLink key={index} href={link.href}>
-								{link.text}
-							</NavbarLink>
-						))}
+					{/* Wrap the navigation links in a container */}
+					<div className="flex pt-4 overflow-scroll w-full justify-center">
+						<div className="flex space-x-8 justify-center">
+							{NAV_LINKS.map((link, index) => (
+								<NavbarLink key={index} href={link.href}>
+									{link.text}
+								</NavbarLink>
+							))}
+						</div>
 					</div>
 				</div>
 			</nav>
 		</header>
+
 	);
 };
 
