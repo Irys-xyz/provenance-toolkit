747ecb0f3f31335f562e2ec7b77dbb88beb433f5
Luke Cassady-Dorion <luke@SPStories.com>
2023-08-22 08:38:19 +0700

    refactored code to use .env.local file

--- app/utils/getRpcUrl.ts
+++ app/utils/getRpcUrl.ts
@@ -1,3 +1,10 @@
+/**
+ * Helper function to return the RPC URL associated with the given currency.
+ * All values are pulled from the .env.local file
+ *
+ * @param currency The name of the currency used.
+ * @returns
+ */
 function getRpcUrl(currency: string): string | undefined {
 	const RPC_URLS: Record<string, string | undefined> = {
 		aptos: process.env.NEXT_PUBLIC_RPC_APTOS,
