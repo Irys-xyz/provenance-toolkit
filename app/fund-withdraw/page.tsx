b9c429653578b205857399c31038fd143236062f
TheHoltz <holtzs.william@gmail.com>
2023-08-25 00:10:43 -0300

    fix: :bug: text style improvements

--- app/fund-withdraw/page.tsx
+++ app/fund-withdraw/page.tsx
@@ -14,24 +14,24 @@ const Page: FC = () => {
 			<div className="flex flex-col text-xs space-y-1 items-start">
 				<h1 className="text-2xl font-bold rounded-xl mb-3">Usage example:</h1>
 
-				<div className="flex flex-col gap-3">
+				<div className="flex flex-col gap-4">
 					<div className="flex flex-col gap-2">
 						<p className="text-base text-neutral-700">To fix the node:</p>
-						<code className="rounded bg-slate-300 px-2 py-1">
+						<code className="rounded bg-[#D8CFCA] px-2 py-1">
 							{'<FundWithdraw config={{ node: "https://node1.bundlr.network" }} />'}
 						</code>
 					</div>
 					<div className="flex flex-col gap-2">
 						<p className="text-base text-neutral-700">To fix the currency:</p>
-						<code className="rounded bg-slate-300 px-2 py-1">{'<FundWithdraw config={{ currency: "ethereum" }} />'}</code>
+						<code className="rounded bg-[#D8CFCA] px-2 py-1">{'<FundWithdraw config={{ currency: "ethereum" }} />'}</code>
 					</div>
 					<div className="flex flex-col gap-2">
 						<p className="text-base text-neutral-700">To set component to fund-only:</p>
-						<code className="rounded bg-slate-300 px-2 py-1">{"<FundWithdraw config={{ fundOnly: true }} />"}</code>
+						<code className="rounded bg-[#D8CFCA] px-2 py-1">{"<FundWithdraw config={{ fundOnly: true }} />"}</code>
 					</div>
 					<div className="flex flex-col gap-2">
 						<p className="text-base text-neutral-700">To set the component to withdraw-only:</p>
-						<code className="rounded bg-slate-300 px-2 py-1">{"<FundWithdraw config={{ withdrawOnly: true }} />"}</code>
+						<code className="rounded bg-[#D8CFCA] px-2 py-1">{"<FundWithdraw config={{ withdrawOnly: true }} />"}</code>
 					</div>
 				</div>
 			</div>
