b570e0439f972fdeb1f9b17b42bbd22ed2dd2353
Luke Cassady-Dorion <luke@SPStories.com>
2023-08-29 08:29:59 +0700

    added connect wallet button

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -1,34 +1,75 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import Spinner from "./Spinner";
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
-    children?: React.ReactNode;
-    scheme?: ButtonScheme;
+	children?: React.ReactNode;
+	scheme?: ButtonScheme;
 }
 
 export enum ButtonScheme {
-    black = "black",
-    white = "white",
+	black = "black",
+	white = "white",
 }
 
-const Button: React.FC<ButtonProps> = ({ children, scheme, ...props }) => {
+const Button: React.FC<ButtonProps> = ({ children, scheme, onClick, ...props }) => {
+	const [connected, setConnected] = useState(false);
+	const [connecting, setConnecting] = useState(false);
 
-    if (!scheme) scheme = ButtonScheme.black
+	useEffect(() => {
+		const checkConnection = async () => {
+			//@ts-ignore
+			const provider = new ethers.providers.Web3Provider(window.ethereum);
+			const accounts = await provider.listAccounts();
+			setConnected(accounts && accounts.length > 0);
+		};
 
-    return (
-        <button
-            className={`
-                w-full justify-center text-md flex items-center gap-2 rounded-full px-4 py-3 font-robotoMono uppercase hover:font-bold lg:px-6 lg:py-5
-                ${{
-                    [ButtonScheme.black]: "bg-black text-white",
-                    [ButtonScheme.white]: "bg-white text-black",
-                }[scheme]
-                }
-`}
-            {...props}
-        >
-            {children}
-        </button>
-    );
-};
+		checkConnection();
+	}, []);
+
+	const handleConnect = async (event: React.MouseEvent) => {
+		event.preventDefault();
+		console.log("handleConnect called");
+
+		setConnecting(true);
+		try {
+			//@ts-ignore
+			const provider = new ethers.providers.Web3Provider(window.ethereum);
+			await provider.send("eth_requestAccounts", []);
+			setConnected(true);
+		} catch (error) {
+			console.error("Failed to connect:", error);
+		}
+		setConnecting(false);
+	};
+
+	const handleButtonClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+		if (connected && onClick) {
+			onClick(event);
+		} else {
+			handleConnect(event);
+		}
+	};
+
+	if (!scheme) scheme = ButtonScheme.black;
+
+	return (
+		<button
+			{...props}
+			type="button"
+			className={`
+				w-full justify-center text-md flex items-center gap-2 rounded-full px-4 py-3 font-robotoMono uppercase hover:font-bold lg:px-6 lg:py-5
+				${
+					{
+						[ButtonScheme.black]: "bg-black text-white",
+						[ButtonScheme.white]: "bg-white text-black",
+					}[scheme]
+				}
+			`}
+			onClick={handleButtonClick}
+		>
+			{!connected ? connecting ? <Spinner color="text-background" /> : "Connect Wallet" : children}
+		</button>
+	);
+}; // Button
 
 export default Button;
