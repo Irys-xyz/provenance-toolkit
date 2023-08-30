cab820e4f836c0eeb8eabdef2118f11352477721
Luke Cassady-Dorion <luke@SPStories.com>
2023-08-30 10:26:23 +0700

    updated main index page

--- app/page.tsx
+++ app/page.tsx
@@ -3,45 +3,61 @@ import Image from "next/image";
 export default function Home() {
 	return (
 		<div className="py-10 bg-background text-text flex flex-col justify-center items-center p-8">
-			<div className="text-2xl font-semibold mb-6 mt-10 leading-relaxed max-w-3xl">
-				The Bundlr Provenance Toolkit is a NextJS project to help you rapidly build dApps using Bundlr.
+			<div className="max-w-3xl w-full mt-5">
+				<div className="relative w-full pb-[56.25%]">
+					<iframe
+						className="absolute top-0 left-0 w-full h-full"
+						src="https://www.youtube.com/embed/RWLKUa34VMQ"
+						frameBorder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+						allowFullScreen
+					></iframe>
+				</div>
 			</div>
-			<div className="text-xl mb-6 leading-relaxed max-w-3xl text-neutral-600">
-				To build with the toolkit, clone or fork the repository and use the components to build your projects.
-				The simple UI design allows for heavy customization when adding to your own projects.
+			<div className="text-2xl font-semibold mt-10 leading-relaxed max-w-3xl">
+				<a
+					href="http://docs.bundlr.network/developer-docs/provenance-toolkit"
+					target="_blank"
+					className="text-blue-500 hover:text-blue-700 underline"
+				>
+					The Bundlr Provenance Toolkit
+				</a>{" "}
+				is a set of UI components to help kickstart your next project.
 			</div>
-			<div className="leading-relaxed max-w-3xl">
-				The toolkit contains the following components. To learn about how they're built, we have per-component
-				tutorials on our docs site.
+			<div className="text-xl mb-6 leading-relaxed max-w-3xl mt-5">
+				To build with the toolkit, clone or fork{" "}
+				<a
+					href="https://github.com/Bundlr-Network/provenance-toolkit"
+					target="_blank"
+					className="text-blue-500 hover:text-blue-700 underline"
+				>
+					the repository
+				</a>{" "}
+				and use the components to build your project.
+				<span>The toolkit contains the following components.</span>
 				<ul className="list-disc mt-4 ml-4">
 					<li>
-						Fund / Withdraw:<span className="text-neutral-600"> Fund nodes and withdraw excess funds.</span>
+						Fund / Withdraw:<span className=""> Manages node balances.</span>
 					</li>
 					<li>
-						Uploader:<span className="text-neutral-600"> Upload single files or groups of files.</span>
+						Uploader:<span className=""> Upload single files or groups of files.</span>
 					</li>
 					<li>
 						Progress Bar Uploader:
-						<span className="text-neutral-600">
-							{" "}
-							Upload large files, and provide feedback with a progress bar.
-						</span>
+						<span className=""> Upload large files, and provide feedback with a progress bar.</span>
 					</li>
 					<li>
-						UDL Uploader:<span className="text-neutral-600"> Upload files and attach a UDL.</span>
+						UDL Uploader:<span className=""> Upload files and attach a UDL.</span>
 					</li>
 					<li>
-						Gasless Uploader:<span className="text-neutral-600"> Pay for user uploads server-side.</span>
+						Gasless Uploader:<span className=""> Pay for user uploads server-side.</span>
 					</li>
 					<li>
 						Transaction Feed:
-						<span className="text-neutral-600">
-							{" "}
-							Query Bundlr nodes for transactions that meet your criteria.
-						</span>
+						<span className=""> Query Bundlr transactions.</span>
 					</li>
 				</ul>
 			</div>
 		</div>
 	);
-} // Hom
+} // Home
