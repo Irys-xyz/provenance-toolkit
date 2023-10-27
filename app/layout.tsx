772d1331c61264dd8058309a114688d15e54a409
Luke Cassady-Dorion <luke@SPStories.com>
2023-10-27 11:40:45 +0200

    added google analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,12 @@ import "@fontsource/roboto-mono/600.css";
 import "@fontsource/roboto-mono/700.css";
 
 import { Inter, Roboto } from "next/font/google";
+import Script from "next/script";
 
 import type { Metadata } from "next";
 import Navbar from "./components/Navbar";
+
+const GTM_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 
 export const metadata: Metadata = {
 	title: "Irys Provenance Toolkit",
@@ -30,6 +33,17 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
 	return (
 		<html lang="en" className={`${roboto.className} bg-background relative`}>
+			{GTM_ID && (
+				<Script id="google-tag-manager" strategy="afterInteractive">
+					{`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+				</Script>
+			)}
 			<body className={roboto.className}>
 				<Navbar />
 				{children}
