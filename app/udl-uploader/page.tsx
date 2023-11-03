c592da16f365fa364a04d431c1ee5c18f5871ac7
Luke Cassady-Dorion <luke@SPStories.com>
2023-11-03 17:26:21 +0100

    updated per-component docs

--- app/udl-uploader/page.tsx
+++ app/udl-uploader/page.tsx
@@ -5,7 +5,7 @@ const Page: FC = () => {
 	return (
 		<div className="mx-auto py-10 bg-background text-text flex flex-col-reverse gap-10 md:flex-row justify-center items-start">
 			<div className="p-10 w-full md:w-1/3 md:p-0">
-				<UDLUploader showImageView={true} showReceiptView={true} />
+				<UDLUploader />
 			</div>
 
 			<div className="flex flex-col space-y-4 p-5 rounded-lg border">
